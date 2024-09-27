a7768cbee4ce6a981f50d25f16c95ccbc946745d
RupeshAryal <aryalrupesh2074@gmail.com>
2024-09-27 16:09:16 +0545

    updated documentation and model

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # NepaliLyricsClustering
-Clustering of Nepali music artists based on the lyrics from their songs. 
+
+Music is an art and a form of expression that involves Melody, Harmony, Rhythm, instruments and so on. The type of music or what we call a music genre is generally determined by different characteristics such as instruments, rhythms, cultural context or a geographical category. Lyrics maybe used as an identifier of a genre but one can argue that same lyrics can be sung in two different genre while expressing the same thing through language. 
+
+In this project, titled Nepali Lyrics Clustering, I aim to group various Nepali artists based solely on the words they use in their songs. By analyzing the lyrical content, we can explore how closely related or distinct these artists are from one another. This approach not only highlights the richness of Nepali music but also contributes to a deeper understanding of the linguistic and thematic diversity within the genre.Given the unique characteristics of the Nepali language, this analysis is approached as a natural language processing (NLP) task. By leveraging NLP techniques, we can uncover patterns and relationships within the lyrics that may not be immediately apparent through traditional analysis.
+
+![Cover Image](images/lyrics\ Cover.png)
+
+
+
--- lyrics.py
+++ lyrics.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import os
 import csv
 
+import requests
+
 # Load environment variables from the .env file
 load_dotenv()
 
@@ -20,24 +22,54 @@ print(artists[50:])
 # Initialize the Genius API client
 genius = lyricsgenius.Genius(api_key)
 
-# Open a CSV file to store the artist names and lyrics
-with open('artists_lyrics.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    # Write the header row
-    writer.writerow(['Artist', 'Song Title', 'Lyrics'])
-    
-    # Loop through each artist
-    for artist in artists[54:]:
-        print(f"Fetching songs for artist: {artist}")
-        try:
-            # Search for the artist
-            artist_obj = genius.search_artist(artist, max_songs=10) 
-            if artist_obj:
-                for song in artist_obj.songs:
-                    # Write artist name, song title, and lyrics to the CSV file
-                    writer.writerow([artist, song.title, song.lyrics])
-                    print(f"Added song: {song.title} by {artist}")
-        except Exception as e:
-            print(f"An error occurred for artist {artist}: {e}")
 
-print("Lyrics have been successfully saved to 'artists_lyrics.csv'.")
+def get_lyrics():
+    # Open a CSV file to store the artist names and lyrics
+    with open('artists_lyrics.csv', mode='w', newline='', encoding='utf-8') as file:
+
+        writer = csv.writer(file)
+        writer.writerow(['Artist', 'Song Title', 'Lyrics'])
+        
+        for artist in artists[54:]:
+            print(f"Fetching songs for artist: {artist}")
+            try:
+                # Search for the artist
+                artist_obj = genius.search_artist(artist, max_songs=10) 
+                if artist_obj:
+                    for song in artist_obj.songs:
+                        # Write artist name, song title, and lyrics to the CSV file
+                        writer.writerow([artist, song.title, song.lyrics])
+                        print(f"Added song: {song.title} by {artist}")
+            except Exception as e:
+                print(f"An error occurred for artist {artist}: {e}")
+
+    print("Lyrics have been successfully saved to 'artists_lyrics.csv'.")
+
+def get_artist_cover_image():
+    for artist in artists:
+        try:
+            artist_image_url = genius.search_artist(artist_name=artist,   max_songs=1).image_url
+        except Exception as e:
+            artist_image_url = False
+        
+        if artist_image_url:
+            response = requests.get(artist_image_url)
+
+            if response.status_code == 200:
+                image_path = "images/" + artist + ".jpg"
+                with open(image_path, 'wb') as file:
+                    file.write(response.content)
+
+                print(f"Image downloaded successfully for artist: {artist}")
+
+            else:
+                print(f"unable to download image. Status Code: {response.status_code}")
+
+        else:
+            print(f"No image found for artist {artist}")
+
+
+get_artist_cover_image()
+                
+
+
